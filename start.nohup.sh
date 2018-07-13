982d62efb9f1fe0ee2f5218b08c32db44371891e
franck <franck@diabeloop.fr>
2018-07-13 15:21:29 +0200

    switch to nohup and rename file

--- start.nohup.sh
+++ start.nohup.sh
@@ -3,11 +3,11 @@
 . "${NVM_DIR}/nvm.sh"
 . version.sh
 
-service=seagull
+service=${1:-'seagull'}
 
 nvm ls "${START_NODE_VERSION}" > /dev/null || { echo "ERROR: Node version ${START_NODE_VERSION} not installed"; exit 1; }
 nvm use --delete-prefix "${START_NODE_VERSION}"
 
 . config/env.sh
 
-forever -a -o ../$service.log -e ../$service.error.log --uid $service start lib/index.js
+nohup node lib/index.js > ../$service.log 2> ../$service.error.log <&- &
