5adf4b5d01ac6c7311cf8fa2c91108568e39af81
Fabrice Sougey <33152621+BigFab@users.noreply.github.com>
2020-05-27 15:32:12 +0200

    PT-998 Write openapi doc (#18)

---
+++ swaggerDef.js
@@ -0,0 +1,20 @@
+const pjson = require('./package.json');
+
+module.exports = 
+{
+  "openapi": "3.0.0",
+  "info": {
+    "title": "Seagull API",
+    "version": pjson.version,
+    "description": "The Diabeloop API for managing user metadata through collections",
+    "license": {
+      "name": "BSD-2-Clause",
+      "url": "https://opensource.org/licenses/BSD-3-Clause"
+    },
+    "contact": {
+      "name": "Diabeloop",
+      "url": "https://www.diabeloop.com",
+      "email": "platforms@diabeloop.fr"
+    }
+  }
+};
