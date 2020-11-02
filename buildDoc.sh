80755076946325d89b08b4154aeca5929a1f2ebf
Fabrice Sougey - Diabeloop <fabrice@diabeloop.fr>
2020-11-02 14:47:25 +0100

    Review PR feedbacks before they even exist

--- buildDoc.sh
+++ buildDoc.sh
@@ -7,6 +7,6 @@ swagger-jsdoc -o doc/openapi/$1-$2-swagger.json -d swaggerDef.js lib/*.js
 
 # If this is not a release candidate but a "true" release, we consider this doc is the latest
 # we create a copy named "latest" to be consumed by documentation website using SwaggerUI
-if [[ ! $2 =~ rc[0-9] ]]; then
+if [[ $2 =~ [0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   cp doc/openapi/$1-$2-swagger.json doc/openapi/$1-latest-swagger.json
 fi
